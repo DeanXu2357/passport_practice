f34332db008a2a8ee21565d401798ed3a30293e6
Dean <jasugun0000@gmail.com>
2018-01-18 15:31:24 +0800

    add basic auth and frontend page

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,5 +16,28 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function () {
-    return 'test';
+    return 'test success';
+});
+
+Route::get('/test-login-faild', function (){
+    return 'login failed';
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/x-23-success', function () {
+    return 'yeah u access success';
+});
+
+Route::get('/iwantlogin', function () {
+    $query = http_build_query([
+        'client_id' => '4',
+        'redirect_uri' => 'http://127.0.0.1:666/test-login-faild',
+        'response_type' => '',
+        'scope' => '',
+    ]);
+
+    return redirect('/oauth/authorize?' . $query);
 });
